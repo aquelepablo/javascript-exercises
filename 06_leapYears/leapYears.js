caafa1af3cec67b148a124f774db24269d0f0163
Pablo Alves <74788322+aquelepablo@users.noreply.github.com>
2023-11-14 14:52:51 +0000

    Exercise 6 ok

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,4 +1,24 @@
-const leapYears = function() {
+const leapYears = function(intYear) {
+
+    let boolLeapYear = false;
+
+    const DIVIDE_BY_100 = 100;
+    const DIVIDE_BY_400 = 400;
+    const DIVIDE_BY_4 = 4;
+
+    if(!Number.isInteger(intYear)) return 'ERROR';
+
+    //YES: intYear % 4 = 0
+    //NOT: intYear % 100 = 0 && intYear % 400 = 0
+
+    if (intYear % DIVIDE_BY_100 == 0) {
+        if (intYear % DIVIDE_BY_400 == 0)
+            boolLeapYear = true; 
+    }else if(intYear % DIVIDE_BY_4 == 0){
+        boolLeapYear = true;
+    }
+
+    return boolLeapYear;
 
 };
 
