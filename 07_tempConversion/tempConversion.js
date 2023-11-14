0634ca225f2ee84ab5a1eeaeeea7faf93e957703
Pablo Alves <74788322+aquelepablo@users.noreply.github.com>
2023-11-14 15:16:17 +0000

    Exercise 7 ok

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,25 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempFahrenheit) {
+  
+  let tempCelsius = 0;
+
+  tempCelsius = (tempFahrenheit - 32) * (5/9);
+  tempCelsius = parseFloat(tempCelsius.toFixed(1));
+
+  console.log(tempFahrenheit + 'ºF' + ' - ' + tempCelsius + 'ºC')
+  return tempCelsius;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempCelsius) {
+
+  let tempFahrenheit = 0;
+
+  tempFahrenheit = (tempCelsius * (9/5) + 32);
+  tempFahrenheit = parseFloat(tempFahrenheit.toFixed(1));
+  
+  console.log(tempFahrenheit + 'ºF' + ' - ' + tempCelsius + 'ºC')
+
+  return tempFahrenheit;
+
 };
 
 // Do not edit below this line
