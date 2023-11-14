23f45fb8fca4a08feb00267681db65990e798db9
Pablo Alves <74788322+aquelepablo@users.noreply.github.com>
2023-11-14 09:37:45 +0000

    Exercise 2 ok

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
+const repeatString = function( strValue, intValue) {
+    
+    if(intValue < 0) return 'ERROR';
+    
+    let strReturn = '';
+    for(i = 0; i < intValue; i++){
+        strReturn += strValue;
+    }
 
+    return strReturn;
 };
 
 // Do not edit below this line
