6b0e85efe3da858ab3d316a24e09544fcf0e68b5
Pablo Alves <74788322+aquelepablo@users.noreply.github.com>
2023-11-14 10:28:48 +0000

    Exercise 4 ok

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,20 @@
-const removeFromArray = function() {
+const removeFromArray = function(...args) {
 
+    if (!Array.isArray(args[0])) return 'ERROR';
+
+    let arrOriginal = args[0];
+
+    for(i = 1; i <= args.length - 1; i++){
+        let intIndex = arrOriginal.findIndex((value) => {
+            return value === args[i];
+        })
+
+        if(intIndex > -1) {            
+            arrOriginal.splice(intIndex, 1);
+        }
+    }
+
+    return arrOriginal;
 };
 
 // Do not edit below this line
